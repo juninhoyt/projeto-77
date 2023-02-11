0cf8afbd660fd91989bea3ed83dc7142ef1d80be
juninhoyt <113396677+juninhoyt@users.noreply.github.com>
2023-02-11 09:57:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("WEB CONTAS");
+var primeiro_nome = "MARCO ANTÔNIO";
+var segundo_nome = "JUNINHO_YT";
+var nome = primeiro_nome.concat(segundo_nome);
+console.log(nome);
+
+var M = 10;
+var J = 11;
+var add = M + J;
+console.log(add);
+var men = M - J;
+console.log(men);
+var vez = M * J;
+console.log (vez);
+var div = M / J;
+console.log (div);
